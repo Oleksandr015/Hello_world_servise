e9e44ed399e8f874638ea14fede5ffc23194eb3d
Oleksandr <iskander@kursGit.pl>
2021-03-22 15:22:17 +0100

    Create index.html, add render in views.py

--- mysite/hello_world/views.py
+++ mysite/hello_world/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
 
 
 def index(request):
-    return HttpResponse("Hello World! This is home page.")
+    today = datetime.datetime.now().date()
+    return render(request, "index.html", {"today", today})
 
 
